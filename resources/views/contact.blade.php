f5ecd16fd8bed9b43cc4d465b5e4cef09ef377aa
Eric Van Johnson <eric@phparch.com>
2024-10-16 20:09:35 -0700

    Some tailwind Stuff

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,29 +1,19 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <div>
+        <x-slot name="header">
             {{ __('Reports') }}
-        </h2>
-    </x-slot>
+        </x-slot>
 
 
-<div>
-    <table class="border-collapse border border-slate-500">
-    <tr>
-        <td>ID</td>
-        <td>{{ $contact->id }}</td>
-    </tr>
-    <tr>
-        <td>Name</td>
-        <td>{{ $contact->first_name }} {{ $contact->last_name }}</td>
-    </tr>
-    <tr>
-        <td>Owner ID</td>
-        <td>{{ $contact->owner_id }}</td>
-    </tr>
-    <tr>
-        <td>Folder</td>
-        <td>{{ $contact->folder }}</td>
-    </tr>
-    </table>
-</div>
+        <div class="border-2 border-amber-500 rounded-2xl p-4 m-4 bg-gray-50 shadow-2xl text-2xl w-1/2 mx-auto">
+            <h2 class=" font-semibold leading-7 text-gray-900">ID</h2>
+            <p class="mt-1  leading-6 text-gray-600 border-b-2 border-gray-100 mb-2 p-4">{{ $contact->id }}</p>
+            <h2 class=" font-semibold leading-7 text-gray-900">Name</h2>
+            <p class="mt-1  leading-6 text-gray-600 border-b-2 border-gray-100 mb-2 p-4">{{ $contact->first_name }} {{ $contact->last_name }}</p>
+            <h2 class=" font-semibold leading-7 text-gray-900">Owner ID</h2>
+            <p class="mt-1  leading-6 text-gray-600 border-b-2 border-gray-100 mb-2 p-4">{{ $contact->owner_id }}</p>
+            <h2 class=" font-semibold leading-7 text-gray-900">Folder</h2>
+            <p class="mt-1  leading-6 text-gray-600 p-4">{{ $contact->folder }}</p>
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+<body class="font-sans antialiased flex flex-col min-h-screen">
+<div class="flex-grow bg-gray-100">
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
-
-        <div class="min-h-screen bg-gray-100">
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <!-- Page Heading -->
+    @if (isset($header))
+        <div class="max-w-7xl p-6 lg:px-8 text-6xl font-extrabold text-center mx-auto">
+            {{ $header }}
         </div>
+    @endif
 
-        @stack('modals')
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+</div>
 
-        @livewireScripts
-    </body>
+<footer class="bg-gray-800 text-white py-4">
+
+    <div
+        class="flex flex-col md:flex-row justify-between items-center pr-4 pl-4">
+        <nav class="flex flex-wrap justify-center space-x-6">
+            <a href="https://phparch.com"
+               class="font-normal text-sm hover:text-gray-400 dark:text-white dark:hover:text-tek-orange-900 mb-2">
+                <img class="h-10" src="https://cdn.phparch.social/logos/php%5Barchitect%5D%20Logo%202023.png"
+                     alt="PHP Architect, LLC."></a>
+        </nav>
+        <p class="text-sm text-center md:text-right">
+            PHP Architect Magazine. For Developer. By Developer.
+        </p>
+
+        <p class="text-sm text-center md:text-right">
+            &copy; {{ date('Y') }} PHP Architect LLC. All rights reserved.
+        </p>
+    </div>
+</footer>
+
+@stack('modals')
+@livewireScripts
+</body>
 </html>
